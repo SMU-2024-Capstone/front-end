61b83bb0a09a536524864127e8cc1554c53fd258
YeongHyeonHeo <djdjfug@naver.com>
2024-09-12 21:17:50 +0900

    CompleteButton 응답 로컬에  저장

--- src/components/Common/CompleteButton.jsx
+++ src/components/Common/CompleteButton.jsx
@@ -101,6 +101,15 @@ const CompleteButton = (props) => {
       })
       .then((data) => {
         console.log(data);
+
+        // ai_recommend 배열의 각 요소를 로컬 스토리지에 저장
+        data.ai_recommend.forEach((item, index) => {
+          const idx = index + 1; // 인덱스를 1부터 시작
+          localStorage.setItem(`tag${idx}`, item.tag);
+          localStorage.setItem(`placename${idx}`, item.placename);
+          localStorage.setItem(`category${idx}`, item.category);
+          localStorage.setItem(`URL${idx}`, item.URL);
+        });
       })
       .catch((error) => {
         console.error("취향 카테고리 선택 중 오류 발생:", error);
